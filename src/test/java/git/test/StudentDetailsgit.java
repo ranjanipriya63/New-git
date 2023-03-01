0380ef19e620f88a64ff52bfe49bfe118771f7b0
ranjanipriya63 <ranjanipriya63ammu@gmail.com>
2023-03-01 23:10:50 +0530

    first push

---
+++ src/test/java/git/test/StudentDetailsgit.java
@@ -0,0 +1,14 @@
+package git.test;
+
+public class StudentDetailsgit {
+	private void name() {
+		System.out.println("Ranjanipriya");
+	}
+	private void course() {
+	System.out.println("Java-Selenium");
+	}
+	private void duration() {
+	System.out.println("3 Months");
+	}
+
+}
